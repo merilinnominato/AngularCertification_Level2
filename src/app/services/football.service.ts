4bd2af2bb39c0a25738f26669c597ef84efa945c
Merili Innominato <xlin24@informatica.aci.it>
2023-12-01 17:16:28 +0100

    fix

--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -24,40 +24,40 @@ export class FootballService {
 
   getStandingsForCurrentSeason(year:number, idLeague:number){
 
-    return this.httpClient.get<Results>(this.standings)
+   // return this.httpClient.get<Results>(this.standings)
 
-  /*   const params = new HttpParams()
+    const params = new HttpParams()
     .set('season', year.toString())
     .set('league', idLeague.toString());
 
-    return this.httpClient.get<Results>(environment.api + '/standings' , {headers: this.headers, params: params} )  */
+    return this.httpClient.get<Results>(environment.api + '/standings' , {headers: this.headers, params: params} ) 
 
   }
 
   getFixitureForTeam(team:number, season:number, league:number, last:number){
 
-    return this.httpClient.get<Results>(this.fixitures)
+  //  return this.httpClient.get<Results>(this.fixitures)
 
 
-   /*  const params = new HttpParams()
+     const params = new HttpParams()
     .set('team', team.toString())
     .set('season', season.toString())
     .set('league', league.toString())
     .set('last', last.toString());
 
-    return this.httpClient.get<Results>(environment.api + '/fixtures' , {headers: this.headers, params: params} ) */
+    return this.httpClient.get<Results>(environment.api + '/fixtures' , {headers: this.headers, params: params} ) 
 
   }
 
 
   getTeamInformation(team:number) {
-    return this.httpClient.get<Results>(this.team)
+   // return this.httpClient.get<Results>(this.team)
 
-    /* const params = new HttpParams()
+     const params = new HttpParams()
     .set('id', team.toString())
  
 
-    return this.httpClient.get<Results>(environment.api + '/teams' , {headers: this.headers, params: params} ) */
+    return this.httpClient.get<Results>(environment.api + '/teams' , {headers: this.headers, params: params} ) 
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   api: 'https://v3.football.api-sports.io',
   apiKey: '33145269a4d78841bb36b1a8d1dad8c0',
-  host: 'v3.football.api-sports.io',
+  host: 'v3.football.api-sports.io'
 };
 
 /*
